4b1c34c3adff9c8a4c13bf3138f5a76fd7f0b833
jordy-love <nonono220@gmail.com>
2021-07-19 20:26:26 +0900

    퀴즈앱 클론코딩

--- lib/screen/screen_home.dart
+++ lib/screen/screen_home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/model/model_quiz.dart';
+import 'package:quiz_app/screen/screen_quiz.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -10,9 +11,21 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   List<Quiz> quizes = [
-    Quiz.fromMap({'title' : 'test', 'candidates': ['a', 'b', 'c', 'd'], 'answer': 0}),
-    Quiz.fromMap({'title' : 'test', 'candidates': ['a', 'b', 'c', 'd'], 'answer': 0}),
-    Quiz.fromMap({'title' : 'test', 'candidates': ['a', 'b', 'c', 'd'], 'answer': 0}),
+    Quiz.fromMap({
+      'title': 'test',
+      'candidates': ['a', 'b', 'c', 'd'],
+      'answer': 0
+    }),
+    Quiz.fromMap({
+      'title': 'test',
+      'candidates': ['a', 'b', 'c', 'd'],
+      'answer': 0
+    }),
+    Quiz.fromMap({
+      'title': 'test',
+      'candidates': ['a', 'b', 'c', 'd'],
+      'answer': 0
+    }),
   ];
 
   @override
@@ -21,61 +34,48 @@ class _HomeScreenState extends State<HomeScreen> {
     double width = screenSize.width;
     double height = screenSize.height;
 
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          title: Center(child: Text("My Quiz App")),
-          backgroundColor: Colors.deepPurple,
-          //leading: Container(), // 뒤로가기버튼 지움
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Center(
-              child: Image.asset(
-                'images/jordy_quiz.jpg',
-                width: width * 0.6,
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(width * 0.024),
-            ),
-            Text(
-              '플러터 퀴즈 앱',
-              style: TextStyle(
-                  fontSize: width * 0.065, fontWeight: FontWeight.bold),
-            ),
-            Text(
-              '퀴즈를 풀기 전 안내사항 입니다.\n꼼꼼히 읽고 퀴즈 풀기를 눌러주세요.',
-              textAlign: TextAlign.center,
-            ),
-            Padding(
-              padding: EdgeInsets.all(width * 0.048),
-            ),
-            _buildStep(width, '1. 랜덤으로 나오는 퀴즈 3개를 풀어보세요.'),
-            _buildStep(width, '2. 문제를 잘 읽고 정답을 고른 뒤\n다음 문제 버튼을 눌러주세요.'),
-            _buildStep(width, '3. 만점을 향해 도전해보세요!'),
-            SizedBox(
-              height: width * 0.048,
-            ),
-            SizedBox(
-              width: width * 0.9,
-              child: ElevatedButton(
-                child: Text(
-                  "지금 퀴즈 풀기",
-                  style: TextStyle(color: Colors.white),
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            title: Center(child: Text("My Quiz App")),
+            backgroundColor: Colors.deepPurple,
+            //leading: Container(), // 뒤로가기버튼 지움
+          ),
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Center(
+                child: Image.asset(
+                  'images/jordy_quiz.jpg',
+                  width: width * 0.6,
                 ),
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.purple,
-                ),
-                onPressed: () {},
               ),
-            ),
-            /*
-            Container(
-              padding: EdgeInsets.all(width * 0.048),
-              child: Center(
+              Padding(
+                padding: EdgeInsets.all(width * 0.024),
+              ),
+              Text(
+                '플러터 퀴즈 앱',
+                style: TextStyle(
+                    fontSize: width * 0.065, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                '퀴즈를 풀기 전 안내사항 입니다.\n꼼꼼히 읽고 퀴즈 풀기를 눌러주세요.',
+                textAlign: TextAlign.center,
+              ),
+              Padding(
+                padding: EdgeInsets.all(width * 0.048),
+              ),
+              _buildStep(width, '1. 랜덤으로 나오는 퀴즈 3개를 풀어보세요.'),
+              _buildStep(width, '2. 문제를 잘 읽고 정답을 고른 뒤\n다음 문제 버튼을 눌러주세요.'),
+              _buildStep(width, '3. 만점을 향해 도전해보세요!'),
+              SizedBox(
+                height: width * 0.048,
+              ),
+              SizedBox(
+                width: width * 0.9,
                 child: ElevatedButton(
                   child: Text(
                     "지금 퀴즈 풀기",
@@ -84,12 +84,37 @@ class _HomeScreenState extends State<HomeScreen> {
                   style: ElevatedButton.styleFrom(
                     primary: Colors.purple,
                   ),
-                  onPressed: () {},
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => QuizScreen(
+                          quizs: quizes,
+                        ),
+                      ),
+                    );
+                  },
                 ),
               ),
-            )
-             */
-          ],
+              /*
+              Container(
+                padding: EdgeInsets.all(width * 0.048),
+                child: Center(
+                  child: ElevatedButton(
+                    child: Text(
+                      "지금 퀴즈 풀기",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.purple,
+                    ),
+                    onPressed: () {},
+                  ),
+                ),
+              )
+               */
+            ],
+          ),
         ),
       ),
     );
